0b63f6d8e2ffa2a2f8d63081b89162041f51bdfa
Aloshychev Ihor <hiranyagarbha@i.ua>
2023-08-07 18:31:00 +0300

    added add author bootstrap

--- library/author/forms.py
+++ library/author/forms.py
@@ -6,3 +6,8 @@ class AuthorForm(forms.ModelForm):
     class Meta:
         model = Author
         fields = ["name", "surname", "patronymic"]
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'surname': forms.TextInput(attrs={'class': 'form-control'}),
+            'patronymic': forms.TextInput(attrs={'class': 'form-control'}),
+        }
